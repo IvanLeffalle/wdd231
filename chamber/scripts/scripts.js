da5f5a270217c4b7e6e8d0a0528f5b7e3a2878b8
IvanLeffalle <ivanmleffalle@gmail.com>
2024-11-16 18:05:35 -0300

    normalize css and responsive

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -174,3 +174,33 @@ document.getElementById('list-toggle').addEventListener('click', () => {
 });
 
 jsonFetch(url).then(displayMember);
+
+
+
+/*Responsive Menu*/
+const menu = document.querySelector("#menu");
+const list = document.querySelector(".list");
+
+menu.addEventListener("click", () => {
+    list.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+const buttons = document.querySelectorAll('.buttons');
+
+const activePage = localStorage.getItem('activePage');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', function (event) {
+        buttons.forEach(button => button.classList.remove('new-active'));
+        this.classList.add('new-active');
+        localStorage.setItem('activePage', this.href);
+    });
+});
+
+if (activePage) {
+    const activeBtn = [...buttons].find(btn => btn.href === activePage);
+    if (activeBtn) {
+        activeBtn.classList.add('new-active');
+    }
+}
